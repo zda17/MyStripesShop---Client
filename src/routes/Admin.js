706b89b89cde4a0b76ff2f32cf409ceda1638182
Morgan Houston <morgan.houston@okstate.edu>
2020-08-24 17:56:58 -0500

    temp plus button added

--- src/routes/Admin.js
+++ src/routes/Admin.js
@@ -8,6 +8,9 @@ import '../stylesheets/Admin.scss';
 //components
 import Header from '../components/Header';
 
+//assets
+import Add1 from '../utils/images/add 1.png';
+
 const Admin = () => {
 
     const [activeSection, setActiveSection] = useState('new');
@@ -21,9 +24,16 @@ const Admin = () => {
                 hClass="Admin-Header"
             />
             <div className="admin-tab-container">
-                <button className={activeSection === 'new' ? "admin-tab-item is-active" : "admin-tab-item"} onClick={() => setActiveSection("new")}><span>New</span></button>
-                <button className={activeSection === 'orders' ? "admin-tab-item is-active" : "admin-tab-item"} onClick={() => setActiveSection("orders")}><span>Orders</span></button>
-                <button className={activeSection === 'fulfilled' ? "admin-tab-item is-active" : "admin-tab-item"} onClick={() => setActiveSection("fulfilled")}><span>Fulfilled</span></button>
+                <button className={activeSection === 'new' ? "admin-tab-item is-active" : "admin-tab-item"} onClick={() => setActiveSection("new")} style={{paddingBottom: 15}}>
+                    <img src={Add1} alt="New" />
+                    <span>New</span>
+                </button>
+                <button className={activeSection === 'orders' ? "admin-tab-item is-active" : "admin-tab-item"} onClick={() => setActiveSection("orders")}>
+                    <span>Orders</span>
+                </button>
+                <button className={activeSection === 'fulfilled' ? "admin-tab-item is-active" : "admin-tab-item"} onClick={() => setActiveSection("fulfilled")}>
+                    <span>Fulfilled</span>
+                </button>
             </div>
 
             <section className="Admin-Component">
