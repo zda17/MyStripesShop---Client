9101511c802bcb22d4a253189e2e3dcb672971fd
Morgan Houston <morgan.houston@okstate.edu>
2020-08-24 18:01:12 -0500

    update

--- src/routes/Admin.js
+++ src/routes/Admin.js
@@ -23,15 +23,18 @@ const Admin = () => {
                 divClass="Container-Header"
                 hClass="Admin-Header"
             />
+            
             <div className="admin-tab-container">
                 <button className={activeSection === 'new' ? "admin-tab-item is-active" : "admin-tab-item"} onClick={() => setActiveSection("new")} style={{paddingBottom: 15}}>
                     <img src={Add1} alt="New" />
                     <span>New</span>
                 </button>
                 <button className={activeSection === 'orders' ? "admin-tab-item is-active" : "admin-tab-item"} onClick={() => setActiveSection("orders")}>
+                    <span className="tab-number">0</span>
                     <span>Orders</span>
                 </button>
                 <button className={activeSection === 'fulfilled' ? "admin-tab-item is-active" : "admin-tab-item"} onClick={() => setActiveSection("fulfilled")}>
+                    <span className="tab-number">0</span>
                     <span>Fulfilled</span>
                 </button>
             </div>
