a3098568ac5e085450f56c4be33e37ad93a2b22f
Morgan Houston <morgan.houston@okstate.edu>
2020-08-24 17:39:01 -0500

    buttons for tabs within the dashboard

--- src/routes/Admin.js
+++ src/routes/Admin.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //style
 import '../stylesheets/Admin.scss';
@@ -9,17 +9,30 @@ import '../stylesheets/Admin.scss';
 import Header from '../components/Header';
 
 const Admin = () => {
+
+    const [activeSection, setActiveSection] = useState('new');
+
     return (
-        <div className="Admin-Panel">
+        <section className="Admin-Panel">
             <Header
                 title="Dashboard"
                 headerClass="Other-Header"
                 divClass="Container-Header"
                 hClass="Admin-Header"
             />
+            <div className="admin-tab-container">
+                <button className={activeSection === 'new' ? "admin-tab-item is-active" : "admin-tab-item"} onClick={() => setActiveSection("new")}><span>New</span></button>
+                <button className={activeSection === 'orders' ? "admin-tab-item is-active" : "admin-tab-item"} onClick={() => setActiveSection("orders")}><span>Orders</span></button>
+                <button className={activeSection === 'fulfilled' ? "admin-tab-item is-active" : "admin-tab-item"} onClick={() => setActiveSection("fulfilled")}><span>Fulfilled</span></button>
+            </div>
 
-            
-        </div>
+            <section className="Admin-Component">
+
+
+
+            </section>
+
+        </section>
     );
 };
 
