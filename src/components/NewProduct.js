c6e5a78dfaae6289776a89cdeedc95d697aa1094
Morgan Houston <morgan.houston@okstate.edu>
2020-08-30 01:21:14 -0500

    added color picker
    will need to npm run install

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
+import { SketchPicker } from 'react-color';
+import { render } from 'react-dom';
 import '../stylesheets/NewProduct.scss';
-
+import { findAllByTestId } from '@testing-library/react';
+  
 
 export default function NewProduct() {
 
     const { handleSubmit, register, errors, reset } = useForm();
+    const [color, setColor] = useState('#3cd6bf');
+    const [displayColorPicker, setDisplayColorPicker] = useState(false);
+
+    const handleClick = () => {
+        setDisplayColorPicker(!displayColorPicker);
+    }
+
+    const handleClose = () => {
+        setDisplayColorPicker(false);
+    }
+
+    const colorArray = [];
+
+    const onChange = (updatedColor) => {
+        setColor(updatedColor);
+    };
 
     const onSubmit = (values) => {
         console.log(values)
+    }
+
+    const addColor = () => {
+        colorArray.push(color.hex);
+        console.log(colorArray);
     }
 
     return (
@@ -19,31 +43,40 @@ export default function NewProduct() {
                         <label htmlFor="name">
                             <span>Name</span>
                         </label>
-                        <input type="text" name="name"></input>
+                        <input className="InputText" type="text" name="name"></input>
                     </div>
                     <div className="InputItem">
                         <label htmlFor="colors">
                             <span>Colors</span>
                         </label>
-                        <input type="text" name="colors"></input>
+                        <input className="InputText" type="text" name="colors" value={colorArray} onClick={ handleClick } readOnly></input>
+                        { displayColorPicker ? <div><SketchPicker
+                            color={color}
+                            onChangeComplete={onChange}
+                            onChange={onChange}
+                            disableAlpha={true}
+                            width={300}
+                        />
+                        <button onClick={addColor}>ADD</button></div> : null}
+                        
                     </div>
                     <div className="InputItem">
                         <label htmlFor="sizes">
                             <span>Sizes</span>
                         </label>
-                        <input type="text" name="sizes"></input>
+                        <input className="InputText" type="text" name="sizes"></input>
                     </div>
                     <div className="InputItem">
                         <label htmlFor="price">
                             <span>Price</span>
                         </label>
-                        <input type="text" name="price"></input>
+                        <input className="InputText" type="text" name="price"></input>
                     </div>
                     <div className="InputItem">
                         <label htmlFor="quantity">
                             <span>Quantity</span>
                         </label>
-                        <input type="text" name="quantity"></input>
+                        <input className="InputText" type="number" name="quantity"></input>
                     </div>
                     {/*ADDS TO CART*/}
                     <input
