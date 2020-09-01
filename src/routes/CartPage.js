a1a40be637f6d74c881b90bff5435720fb577c96
Shannon Scirghio <shanscirg7@gmail.com>
2020-08-31 21:31:43 -0700

    implemented localstorage function for generating total + taxes on order summary of checkout page (when on mobile), made cartpage mobile responsive

--- src/routes/CartPage.js
+++ src/routes/CartPage.js
@@ -6,6 +6,7 @@ import '../stylesheets/CartPage.scss';
 import '../stylesheets/Checkout.scss';
 import '../stylesheets/UserInfoForm.scss';
 import { useHistory } from 'react-router-dom';
+import localStorage from '../utils/localStorage';
 
 export default function CartPage() {
     const { cart } = useContext(CartContext);
@@ -21,11 +22,12 @@ export default function CartPage() {
 
     const goToCheckout = () => {
         history.push('/Checkout');
+        localStorage.setTotalPrice(totalPrice + parseInt(((totalPrice) * 0.08).toFixed(2)));
     }
 
     return (
         <>
-            {cart[0] ?
+            {cart && cart[0] ?
                 <section className='cart-page-container'>
                     <h1>CART</h1>
                     {windowWidth > 780 && <hr className='horizontal-line'></hr>}
@@ -34,41 +36,41 @@ export default function CartPage() {
                             {windowWidth > 780 && <li id='product'>PRODUCT</li>}
                             <article className='cart-page-article-item'>
                                 <CartItem
-                                    displayQuantity={windowWidth > 780 ? false : true}
+                                    displayQuantity={windowWidth > 400 ? false : true}
                                     displayRemove={true}
                                     displayTotalProdPrice={windowWidth > 780 ? false : true}
                                 />
                             </article>
                         </ul>
+                        <ul className='quantity-list'>
+                            {windowWidth > 780 &&
+                                <li id='quantity'>QUANTITY</li>}
+                            {windowWidth > 400 && 
+                            cart.map(product =>
+                                <li>
+                                    <article className='cart-page-article quantity-box'>
+                                        <HandleQuantity
+                                            product={product}
+                                        />
+                                    </article>
+                                </li>
+                            )}
+                        </ul>
                         {windowWidth > 780 &&
-                            <>
-                                <ul className='quantity-list'>
-                                    <li id='quantity'>QUANTITY</li>
-                                    {cart.map(product =>
-                                        <li>
-                                            <article className='cart-page-article quantity-box'>
-                                                <HandleQuantity
-                                                    product={product}
-                                                />
-                                            </article>
-                                        </li>
-                                    )}
-                                </ul>
-                                <ul className='item-price-times-quantity'>
-                                    <li id='total'>TOTAL</li>
-                                    {cart.map(product =>
-                                        <article className='cart-page-article'>
-                                            <p className='total-prod-price'>${product.totalProductPrice}</p>
-                                        </article>
-                                    )}
-                                </ul>
-                            </>
+                            <ul className='item-price-times-quantity'>
+                                <li id='total'>TOTAL</li>
+                                {cart.map(product =>
+                                    <article className='cart-page-article'>
+                                        <p className='total-prod-price'>${product.totalProductPrice}</p>
+                                    </article>
+                                )}
+                            </ul>
                         }
                     </section>
                     {windowWidth > 780 &&
                         <hr className='horizontal-line-bottom'></hr>
                     }
-                    <section className='checkout-total-wrapper'>
+                    <section className='cart-total-wrapper'>
                         <h3 className='checkout-total-title'>CART TOTAL: ${totalPrice}<span className='usd'> (USD)</span></h3>
                         <h4>Shipping & taxes calculated at checkout</h4>
                         <button type='button' className='checkout-btn' onClick={goToCheckout}>CHECKOUT</button>
