168ed1c218728a3e3918f9f6ab810e3b488e04fc
Shannon Scirghio <shanscirg7@gmail.com>
2020-09-10 00:27:03 -0700

    handled errors with useForm hook and styled them

--- src/components/Checkout/UserInfoForm.js
+++ src/components/Checkout/UserInfoForm.js
@@ -7,12 +7,13 @@ import Payment from './CheckoutForm';
 
 const UserInfoForm = ({ open }) => {
 
-    const { register, handleSubmit } = useForm();
     const { paid, userInfo, setUserInfo } = useContext(CartContext);
+    const { register, errors, reset, handleSubmit } = useForm();
     const [filledOut, setFilledOut] = useState(false);
     const [showCheckoutForm, setShowCheckoutForm] = useState(false);
 
     const onSubmit = data => {
+        console.log('data', data);
         setUserInfo(data);
         setFilledOut(true);
         setShowCheckoutForm(true);
@@ -20,29 +21,15 @@ const UserInfoForm = ({ open }) => {
         window.scrollTo(0, 0);
     };
 
-    const formInputs = ['Address', 'Apartment, suite, etc. (optional)', 'City']
-
-    const Inputs = () => {
-        return (
-            <>
-                {formInputs.map((inputStr, index) => (
-                    <input
-                        type='text'
-                        placeholder={inputStr}
-                        name={inputStr.toLowerCase().replace(/[, ]+/g, "").replace(/[()]/g, '').trim()}
-                        ref={register}
-                        key={index}
-                    />
-                ))}
-            </>
-        )
-    }
-
     const states = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona",
         "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", " North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
 
     const goBackToForm = () => {
         setFilledOut(false);
+        const { email, firstName, lastName, address, apartment, city, country, state, zipCode, phone } = userInfo;
+        reset({
+            email, firstName, lastName, address, apartment, city, country, state, zipCode, phone
+        })
     }
 
     return (
@@ -50,27 +37,96 @@ const UserInfoForm = ({ open }) => {
             {!filledOut &&
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <h3>Contact Information</h3>
-                    <input type='text' placeholder='Email' name='email' value={userInfo.email} ref={register} />
+                    <input
+                        type='text'
+                        placeholder='Email'
+                        name='email'
+                        ref={register({ required: true, pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ })}
+                    />
+                    {errors.email && errors.email.type === 'required' && <h5>*Email is required.</h5>}
+                    {errors.email && errors.email.type === 'pattern' && <h5>*Invalid email.</h5>}
                     <h3>Shipping Address</h3>
-                    <div className='name-div'>
-                        <input type='text' placeholder='First name (optional)' value={userInfo.firstName} className='name first-name' name='firstName' ref={register} />
-                        <input type='text' placeholder='Last name' value={userInfo.lastName} className='name last-name' name='lastName' ref={register} />
-                    </div>
-                    <Inputs />
-                    <div className='name-div'>
-                        <select name='country' defaultValue={userInfo.country} ref={register} className='country-select' required>
+                    <section className='name-div'>
+                        <input
+                            type='text'
+                            placeholder='First name (optional)'
+                            className='name first-name'
+                            name='firstName'
+                            ref={register}
+                        />
+                        <input
+                            type='text'
+                            placeholder='Last name'
+                            className='name last-name'
+                            name='lastName'
+                            ref={register({ required: true })}
+                        />
+                    </section>
+                    {errors.lastName && <h5>*Last name is required.</h5>}
+                    <input
+                        type='text'
+                        placeholder='Address'
+                        name='address'
+                        ref={register({ required: true })}
+                    />
+                    {errors.address && <h5>*Address is required.</h5>}
+                    <input
+                        type='text'
+                        placeholder='Apartment, suite, etc. (optional)'
+                        name='apartment'
+                        ref={register}
+                    />
+                    <input
+                        type='text'
+                        placeholder='City'
+                        name='city'
+                        ref={register({ required: true })}
+                    />
+                    {errors.city && <h5>*City is required.</h5>}
+                    <section className='name-div'>
+                        <select
+                            name='country'
+                            defaultValue={userInfo.country}
+                            ref={register({ required: true })}
+                            className='country-select'
+                            required
+                        >
                             <option value="" disabled hidden>Country/Region</option>
                             <option value="United States">United States</option>
                         </select>
-                        <select name='state' defaultValue={userInfo.state} className='state-select' ref={register} required>
+                        {errors.country && <h5>*Country is required.</h5>}
+                        <select
+                            name='state'
+                            defaultValue={userInfo.state}
+                            className='state-select'
+                            ref={register({ required: true })}
+                            required
+                        >
                             <option value="" disabled hidden>State</option>
                             {states.map((state, index) => (
                                 <option value={state} key={index}>{state}</option>
                             ))}
                         </select>
-                        <input type='text' placeholder='ZIP code' value={userInfo.zipCode} className='zip' name='zipCode' ref={register} />
-                    </div>
-                    <input type='text' placeholder='Phone' value={userInfo.phone} className='name' name='phone' ref={register} />
+                        {errors.state && <h5>*State is required.</h5>}
+                        <input
+                            type='text'
+                            placeholder='ZIP code'
+                            className='zip'
+                            name='zipCode'
+                            ref={register({ required: true })}
+                        />
+                    </section>
+                    {errors.zipCode && <h5>*ZIP code is required.</h5>}
+                    <input
+                        type='text'
+                        placeholder='Phone'
+                        className='name'
+                        name='phone'
+                        ref={register({ required: true, minLength: 10, pattern: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im })}
+                    />
+                    {errors.phone && errors.phone.type === 'required' && <h5>*Phone number is required.</h5>}
+                    {errors.phone && errors.phone.type === 'minLength' && <h5>*Invalid phone number.</h5>}
+                    {errors.phone && errors.phone.type === 'pattern' && <h5>*Invalid phone number.</h5>}
                     <div className='button-div'>
                         <Link to='/Cart' className='back-btn'>
                             <i class="fa fa-angle-double-left" aria-hidden="true"></i>
@@ -90,10 +146,9 @@ const UserInfoForm = ({ open }) => {
                                     <p><strong>Contact: </strong></p>
                                     <p className='user-email'>{userInfo.email}</p>
                                 </section>
-                                {/* destructure userInfo? */}
                                 <section className='ship-div'>
                                     <p><strong>Ship to: </strong></p>
-                                    <p className='user-ship-info'>{userInfo.address}, {userInfo.apartmentsuiteetc.optional && '#' + userInfo.apartmentsuiteetc.optional + ', '}{userInfo.city}, {userInfo.state}, {userInfo.zipCode}, {userInfo.country}</p>
+                                    <p className='user-ship-info'>{userInfo.address}, {userInfo.apartment && '#' + userInfo.apartment + ', '}{userInfo.city}, {userInfo.state}, {userInfo.zipCode}, {userInfo.country}</p>
                                 </section>
                             </section>
                             <div className='button-div'>
