e7748101e5f8713f645b5a9a93fc1ae31306684a
Morgan Houston <morgan.houston@okstate.edu>
2020-09-20 22:14:33 -0500

    Added date and spacing

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -9,8 +9,8 @@ import axios from '../utils/axios';
 export default function Orders() {
 
     const [activeSection, setActiveSection] = useState('all');
+    const [selected, setSelected] = useState('');
     const [orders, setOrders] = useState([]);
-    const [selected, setSelected] = useState([]);
 
     useEffect(() => {
         axios.get(`/orders/`)
@@ -20,30 +20,35 @@ export default function Orders() {
             });
     }, []);
 
+    const handleClick = (e) => {
+        const index = e.target.key;
+
+    }
+
     return (
         <div className="orders-list">
             {activeSection === selected ?
                 <>
-                    {orders.map(order => (
-                        <article className="order-item">
-                            <div className="order-item-left">
-                                <span>{order.email}</span><br /><br />
-                                <span onClick={() => setSelected(order.uuid)} className="date">09/14/2020</span>
-                            </div>
-                            <div className="order-item-right">
-                                <span>{order.amount_cents}</span><br /><br />
-                                <span>></span>
-                            </div>
-                        </article>
-                    ))}
+                    {orders.filter(order => order.uuid == selected),
+                        console.log(orders),
+                        orders.map((order, index) => (
+                            <article className="order-item" key={index}>
+                                <span>{order.email}</span><br />
+                                <span>{new Date(order.created_at).toLocaleDateString()}</span><span>{order.amount_cents}</span><br />
+                                <span>Order # </span><br />
+                                <span>{order.address + ', ' + order.state}</span><br />
+                                <span>Shipping: Standard</span><br />
+                                <span>{order.product_skus_and_quantity}</span><br />
+                            </article>
+                        ))}
                 </>
                 :
                 <>
-                    {orders.map(order => (
-                        <article className="order-item">
+                    {orders.map((order, index) => (
+                        <article className="order-item" onClick={() => { setSelected(order.uuid); setActiveSection(order.uuid); console.log(selected) }} key={index}>
                             <div className="order-item-left">
                                 <span>{order.email}</span><br /><br />
-                                <span onClick={() => setSelected(order.uuid)} className="date">09/14/2020</span>
+                                <span className="date">{new Date(order.created_at).toLocaleDateString()}</span>
                             </div>
                             <div className="order-item-right">
                                 <span>{order.amount_cents}</span><br /><br />
