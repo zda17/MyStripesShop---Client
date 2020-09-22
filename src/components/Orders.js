66a917c603a62607b0fd8cd3efa8d481216a1f43
Morgan Houston <morgan.houston@okstate.edu>
2020-09-21 20:49:18 -0500

    Optimized for mobile
    Added temp image and working on getting right size if sku has color at end

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -21,6 +21,16 @@ export default function Orders() {
             });
     }, []);
 
+    function getSecondPart(str) {
+        var index = 0;
+        for(var i = 0; i < 5; i++) {
+            if(str.split('-')[i] != undefined) {
+                index = i;
+            }
+        }
+        return str.split('-')[index];
+    }
+
     const handleClick = (e) => {
         setActiveSection('all');
 
@@ -33,11 +43,18 @@ export default function Orders() {
             selectedOrder.map((order, index) => (
                 <article className="order-item" key={index}>
                     <span>{order.email}</span><br />
-                    <span>{new Date(order.created_at).toLocaleDateString()}</span><span>{order.amount_cents}</span><br />
+                    <span>{new Date(order.created_at).toLocaleDateString()}</span><span>{'$' + order.amount_cents}</span><br />
                     <span>Order # {order.id}</span><br />
-                    <span>{order.address + ', ' + order.state}</span><br />
+                    <span>{order.address + ", " + order.state}</span><br />
                     <span>Shipping: Standard</span><br />
-                    <span>{order.product_skus_and_quantity}</span><br />
+                    {order.product_skus_and_quantity.map((item, index) => (
+                        <section className="order-product-item" key={index}>
+                            <div className="img-container">
+                                <img src="https://i.imgur.com/Gn9PPb3.png"></img>
+                            </div>
+                            <span>{"QTY: " + item[1] + "\u000A" + "SIZE: " + getSecondPart(item[0])}</span>
+                        </section>
+                    ))}
                     <button>Fullfill</button>
                 </article>
             ))
@@ -54,7 +71,7 @@ export default function Orders() {
                         <span className="date">{new Date(order.created_at).toLocaleDateString()}</span>
                     </div>
                     <div className="order-item-right">
-                        <span>{order.amount_cents}</span><br /><br />
+                        <span>{'$' + (order.amount_cents / 100).toFixed(2)}</span><br /><br />
                         <span>></span>
                     </div>
                 </article>
