9c937cefd80d43b395833eb6cdf1b9e3194bfd4f
Shannon Scirghio <shanscirg7@gmail.com>
2020-09-30 19:14:21 -0700

    refactored and shortened conditional that gets product size

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -21,48 +21,27 @@ export default function Orders() {
             });
     }, []);
 
-    //There must be a better way, but hey it works :)
-    function getSecondPart(str) {
-        var index = 0;
-        for (var i = 0; i < 5; i++) {
+    // Some product SKUs end in a size, and some end in a color. This makes sure to return the size.
+    const getSecondPart = str => {
+        let index = 0;
+        for (let i = 0; i < 5; i++) {
             if (str.split('-')[i] != undefined) {
                 index = i;
             }
         }
         const last = str.split('-')[index];
-        console.log(last);
-        if ((last == "XXXS") ||
-            (last == "XXS") ||
-            (last == "XS") ||
-            (last == "S") ||
-            (last == "M") ||
-            (last == "L") ||
-            (last == "XL") ||
-            (last == "XXL") ||
-            (last == "XXXL") ||
-            (last == "5") ||
-            (last == "5.5") ||
-            (last == "6") ||
-            (last == "6.6") ||
-            (last == "7") ||
-            (last == "7.5") ||
-            (last == "8") ||
-            (last == "8.5") ||
-            (last == "9") ||
-            (last == "9.5") ||
-            (last == "10") ||
-            (last == "10.5") ||
-            (last == "11") ||
-            (last == "11.5") ||
-            (last == "12") ||
-            (last == "12.5")) {
-            return str.split('-')[index];
-        } else return str.split('-')[index - 1];
+        const sizes = ["XXXS", "XXS", "XS", "S", "M", "L", "XL", "XXL", "XXXL", "5", "5.5", "6", "6.6", "7", "7.5", "8", "8.5", "9", "9.5", "10", "10.5", "11", "11.5", "12", "12.5"]
+        let sizeLast = false;
+        for (let i = 0; i < sizes.length; i++) {
+            if (sizes[i] == last) {
+                sizeLast = true;
+            }
+        }
+        return sizeLast ? str.split('-')[index] : str.split('-')[index - 1];
     }
 
     const handleClick = (e) => {
         setActiveSection('all');
-
     }
 
     //passes the selected orders id to find and update.
@@ -81,24 +60,24 @@ export default function Orders() {
         const selectedOrder = orders.filter(order => order.uuid == selected);
 
         return (
-                <article className="order-item" style={{ borderTop: 'solid 1px rgb(95, 95, 95)', margin: '15px 0px 20px', borderBottom: 'none' }}>
-                    <span>{selectedOrder[0].email}</span><br />
-                    <span style={{ color: 'rgb(95, 95, 95)' }}>{new Date(selectedOrder[0].created_at).toLocaleDateString()}</span><span>{'$' + (selectedOrder[0].amount_cents / 100).toFixed(2)}</span><br />
-                    <span>Order # {selectedOrder[0].id}</span><br />
-                    <span>{selectedOrder[0].address}</span><br />
-                    <span>Shipping: Standard</span><br />
-                    {selectedOrder[0].product_skus_and_quantity.map((item, index) => (
-                        <section className="order-product-item" key={index}>
-                            <div className="img-container">
-                                <img src="https://i.imgur.com/Gn9PPb3.png"></img>
-                            </div>
-                            <span>{"QTY: " + item[1] + "\u000A\u000A" + "SIZE: " + getSecondPart(item[0])}</span>
-                        </section>
-                    ))}
-                    <form onSubmit={handleSubmit} >
-                        <button id={selectedOrder[0].id} type={"submit"}>FULFILLED</button>
-                    </form>
-                </article>
+            <article className="order-item" style={{ borderTop: 'solid 1px rgb(95, 95, 95)', margin: '15px 0px 20px', borderBottom: 'none' }}>
+                <span>{selectedOrder[0].email}</span><br />
+                <span style={{ color: 'rgb(95, 95, 95)' }}>{new Date(selectedOrder[0].created_at).toLocaleDateString()}</span><span>{'$' + (selectedOrder[0].amount_cents / 100).toFixed(2)}</span><br />
+                <span>Order # {selectedOrder[0].id}</span><br />
+                <span>{selectedOrder[0].address}</span><br />
+                <span>Shipping: Standard</span><br />
+                {selectedOrder[0].product_skus_and_quantity.map((item, index) => (
+                    <section className="order-product-item" key={index}>
+                        <div className="img-container">
+                            <img src="https://i.imgur.com/Gn9PPb3.png"></img>
+                        </div>
+                        <span>{"QTY: " + item[1] + "\u000A\u000A" + "SIZE: " + getSecondPart(item[0])}</span>
+                    </section>
+                ))}
+                <form onSubmit={handleSubmit} >
+                    <button id={selectedOrder[0].id} type={"submit"}>FULFILLED</button>
+                </form>
+            </article>
         )
     }
 
@@ -132,7 +111,6 @@ export default function Orders() {
                     <OrderItem />
                 </>
             }
-
         </div>
     )
 }
